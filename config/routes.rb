33b1022ab46a60265d11db31be08b4f7fbc6712a
MissYoruwara <cecel31@free.fr>
2018-08-29 14:29:15 +0200

    ajout favicon + retrait fonctionnalité d'ajout de post

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,10 @@
 Rails.application.routes.draw do
-
-  get "connexion" => 'connection/disconnect'
-
+  
   root 'home#index'
   
   get "presentation" => 'home#presentation'
 
   get "entreprise" => 'home#entreprise'
-
-
-  get "connexion" => 'connection#connect'
-
-  get "posts/add" => 'posts#create'
-  post "posts/add" => 'posts#create'
-
 
   get "post/:id", to: 'posts#show', as: 'posts_show'
 
